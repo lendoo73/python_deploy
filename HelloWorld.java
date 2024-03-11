7eed0eb08c0af1d672941a16c89d1a2e49a1e749
Csaba Cselko <ccselko@icomera.onmicrosoft.com>
2024-03-11 15:06:06 +0000

    Github poll

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins modified again");
+        System.out.println("Hello Jenkins modified again changed to Github poll");
     }
 }
